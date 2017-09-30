e56aac8ffcf499c3bdd977d5acaee02afac2c0e1
tjr05d <tjr05d@my.fsu.edu>
2017-09-30 19:50:06 -0400

    fixes google analytics tag

--- app/assets/javascripts/new_age/google_analytics.js
+++ app/assets/javascripts/new_age/google_analytics.js
@@ -1,5 +1,4 @@
 document.addEventListener('turbolinks:load', function(event) {
-  console.log("google analytivs load"); 
   if (typeof ga === 'function') {
     ga('set', 'location', event.data.url);
     ga('send', 'pageview');
--- app/assets/javascripts/new_age/manifest.js
+++ app/assets/javascripts/new_age/manifest.js
@@ -3,3 +3,5 @@
 //= require turbolinks
 //= require bootstrap
 //= require new_age/new_age
+//= require new_age/google_analytics
+
